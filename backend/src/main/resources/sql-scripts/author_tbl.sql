453b35b4ac95d12970d8dcd75856725b73505a78
Shrestha-Binod <shrbinod@sheridancollege.ca>
2021-03-06 21:41:12 -0500

    sql scripts added

--- backend/src/main/resources/sql-scripts/author_tbl.sql
+++ backend/src/main/resources/sql-scripts/author_tbl.sql
@@ -2,10 +2,17 @@
 use BookLibrary;
 go
 
---DROP TABLE author;
-CREATE TABLE author(
-authorId INTEGER PRIMARY KEY NOT NULL,
-author VARCHAR(50) NOT NULL,
-address VARCHAR(50),
-email VARCHAR(90),
-);
+----DROP TABLE author;
+--CREATE TABLE author(
+--authorId INTEGER PRIMARY KEY IDENTITY NOT NULL,
+--author VARCHAR(50) NOT NULL,
+--address VARCHAR(50),
+--email VARCHAR(90),
+--);
+--ADDING IDENTITY FOR AUTO INCREMENT
+--ALTER TABLE author
+--ADD CONSTRAINT fk_authorId IDENTITY (bookId, authorId);
+--INSERT INTO author VALUES('Binod Shrestha', '6 Berringer Grove', 'abc@abc.com');
+INSERT INTO author VALUES('Aayush Shrestha', '6 Berringer Grove', 'abc@abc.com');
+
+SELECT * FROM AUTHOR;
--- backend/src/main/resources/sql-scripts/book-tbl.sql
+++ backend/src/main/resources/sql-scripts/book-tbl.sql
@@ -1,12 +1,21 @@
 use BookLibrary;
 go
+--exec sp_rename 'book.author_id', 'authorId', 'COLUMN';
 --DROP TABLE dbo.book;
-CREATE TABLE book(
-bookId INTEGER PRIMARY KEY IDENTITY  NOT NULL,
-bookName VARCHAR(50) NOT NULL,
-publishedYear INTEGER,
-publisher VARCHAR(50),
-price MONEY,
-author_id INTEGER,
-CONSTRAINT fk_authorId FOREIGN KEY (bookId) REFERENCES author(authorId)
-);
+--CREATE TABLE book(
+--bookId INTEGER PRIMARY KEY IDENTITY  NOT NULL,
+--bookName VARCHAR(50) NOT NULL,
+--publishedYear INTEGER,
+--publisher VARCHAR(50),
+--price MONEY,
+--authorId INTEGER,
+--CONSTRAINT FK_AuthorBook FOREIGN KEY (authorId) REFERENCES author(authorId)
+--);
+
+--INSERT SOME DUMMY DATA
+--INSERT INTO dbo.book VALUES('SPRING BOOT', 2021, 'OXFORD', 250,100);
+--ALTER TABLE BOOK DROP COLUMN authorId;
+--INSERT INTO book VALUES('JAVA', 2021, 'OXFORD', 245.00, 1);
+INSERT INTO book VALUES('ANGULAR', 2021, 'OXFORD', 245.00, 2);
+
+SELECT * FROM BOOK;
