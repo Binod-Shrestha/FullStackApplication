46cff627be2bf95a5379fbe027055ccad91f1857
Shrestha-Binod <shrbinod@sheridancollege.ca>
2021-03-07 14:30:39 -0500

    add services

---
+++ backend/src/main/java/com/shresthabinod/backend/controllers/BookController.java
@@ -0,0 +1,9 @@
+package com.shresthabinod.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class BookController {
+}
