0f763ec252879749c13a6e7fb60e194f8cb647d9
Binod Shrestha <47705585+Binod-Shrestha@users.noreply.github.com>
2021-03-08 22:09:07 -0500

    Develop (#9)
    * add sql scripts (#2)
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/database connection (#4)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/add components (#8)
    
    * Develop (#5)
    
    * add sql scripts (#2)
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/database connection (#4)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/rest controller (#6)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    * add services
    
    * add rest controller
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/services (#7)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    * add services
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * add annotation
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add components for frontend
    
    * add new components to front end
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { AddItemsComponent } from './components/add-items/add-items.component';
+import { DisplayItemsComponent } from './components/display-items/display-items.component';
+import { UpdateComponentsComponent } from './components/update-components/update-components.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    MainComponent,
+    AddItemsComponent,
+    DisplayItemsComponent,
+    UpdateComponentsComponent
     MainComponent
   ],
   imports: [
---
+++ src/app/components/update-components/update-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-components',
+  templateUrl: './update-components.component.html',
+  styleUrls: ['./update-components.component.scss']
+})
+export class UpdateComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
