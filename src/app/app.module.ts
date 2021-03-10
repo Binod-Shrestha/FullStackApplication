d67facbc7a4bebea3fefa22cb52dfeb39ac61bd5
Binod Shrestha <47705585+Binod-Shrestha@users.noreply.github.com>
2021-03-10 11:33:07 -0500

    Develop (#15)
    * add sql scripts (#2)
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/database connection (#4)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/add components (#8)
    
    * Develop (#5)
    
    * add sql scripts (#2)
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/database connection (#4)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/rest controller (#6)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    * add services
    
    * add rest controller
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/services (#7)
    
    * add sql scripts
    
    * sql scripts added
    
    * create project structure
    
    * create book model
    
    * code clean up
    
    * add db configuration for ms sql server
    
    * add services
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * add annotation
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add components for frontend
    
    * add new components to front end
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>
    
    * Feature/add components (#10)
    
    * add new components to front end
    
    * package json updated with some dependencies
    
    * material dependecy added (#12)
    
    * backend linked with frontend (#14)
    
    Co-authored-by: Shrestha-Binod <shrbinod@sheridancollege.ca>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { DisplayItemsComponent } from './components/display-items/display-items.
 import { UpdateComponentsComponent } from './components/update-components/update-components.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import {BookService} from './service/book.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CdkColumnDef } from '@angular/cdk/table';
 
 
 
@@ -30,9 +33,11 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
     MaterialModule
   ],
-  providers: [],
+  providers: [BookService, CdkColumnDef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Book } from '../model/book';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BookService {
 
 //api uri
-private
+private apiURI: string;
 
-  constructor() { }
+  constructor( private http: HttpClient) {
+   this.apiURI = 'http://localhost:8080/api/books';
+   }
+
+// get the list of books
+findAll(): Observable<Book[]> {
+return this.http.get<Book[]>(this.apiURI);
+}
+save(book: Book) {
+return this.http.post<Book>(this.apiURI, book);
+}
+
 }
