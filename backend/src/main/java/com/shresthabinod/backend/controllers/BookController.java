e8b3160544bbc314feda1dbb2c6aa22a8eb21c72
Binod Shrestha <47705585+Binod-Shrestha@users.noreply.github.com>
2021-03-10 11:26:43 -0500

    backend linked with frontend (#14)

--- backend/src/main/java/com/shresthabinod/backend/controllers/BookController.java
+++ backend/src/main/java/com/shresthabinod/backend/controllers/BookController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class BookController {
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../model/book';
+import { BookService } from '../../service/book.service';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  constructor() { }
-
+books: Book[]=[];
+  constructor( private bookService: BookService) { }
   ngOnInit(): void {
+  this.bookService.findAll().subscribe(data => {
+  this.books = data;
+  })
   }
-
 }
