a149e88143924a1e571adc7378e45ae974cc7b75
Binod Shrestha <shresthabinod.softdev@gmail.com>
2021-03-10 14:13:14 -0500

    custom exception handler modified

--- backend/src/main/java/com/shresthabinod/backend/error/NotFoundException.java
+++ backend/src/main/java/com/shresthabinod/backend/error/NotFoundException.java
@@ -1,8 +1,14 @@
 package com.shresthabinod.backend.error;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends Exception {
 
-  public static String displayNotFoundResourceException(String msg){
-    return msg;
+  private static final long serialVersionUID = 1L;
+
+  public NotFoundException(String msg){
+    super(msg);
   }
 }
