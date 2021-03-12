224bd995474333d15a5b2cf8197c8ae7c9b6c117
Binod Shrestha <shresthabinod.softdev@gmail.com>
2021-03-12 08:07:51 -0500

    add function added to the fornt end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AddItemsComponent } from './components/add-items/add-items.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{ path:'', component: MainComponent},
+  { path:'addbook', component: AddItemsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
